6d09149ad0576ef03b668517d9895e2f5924ee5e
rgoo <info@richardgoodine.com>
2015-09-29 13:41:24 -0500

    Update data.php

--- scripts/data.php
+++ scripts/data.php
@@ -1,1 +1,30 @@
 
+<?php
+
+require_once 'config.php';
+
+try {
+    // create db handle
+    $dbh = new PDO("mysql:host=$host;dbname=$db", $user, $pw);
+
+    // prepare and execute sql 
+    $statement = $dbh->prepare("SELECT * FROM tarot_cards");
+    $statement->execute();
+    // fetch query as associative array
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    // encode query to json so angular can use it
+    $json = json_encode($results);
+    
+    echo $json;
+    
+    // close the database connection 
+    $dbh = null;
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+
+
+?>
