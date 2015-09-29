109473ec3b85de004ab5f3863409873e73cfbe2e
rgoo <info@richardgoodine.com>
2015-09-29 13:48:36 -0500

    Update tarotApp.sql

--- tarotApp.sql
+++ tarotApp.sql
@@ -1,11 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 4.0.4.1
 -- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Sep 28, 2015 at 07:46 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
