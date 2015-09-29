568398d85f627a0e959b1d028e007f910554ccc9
shochiku <bargnani@gmail.com>
2015-09-29 14:07:12 -0500

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tarotng
+
+## Installation
+* import the database (tarotApp.sql) 
+* add db credentials to scripts/config.php 
---
+++ scripts/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Add you db credentials
+ * 
+ */
+
+
+$host = "localhost";
+$db   = "";
+$user = "";
+$pw   = "";
+ 
+?>
