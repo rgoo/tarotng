3640914bc4961754c987535ae635f832af9a6d1e
shochiku <bargnani@gmail.com>
2015-10-06 23:10:26 -0500

    used singleton for db instance

--- scripts/config.php
+++ scripts/config.php
@@ -3,11 +3,12 @@
 /**
  * Add your db credentials
  * 
- */
+ */ 
+$dbDetails = array(
+        'db_name' => '',
+        'db_host' => 'localhost',
+        'db_user' => 'root',
+        'db_pass' => 'root'
+);
 
-$host = "localhost";
-$db   = "";
-$user = "";
-$pw   = "";
- 
 ?>
--- scripts/data.php
+++ scripts/data.php
@@ -3,28 +3,65 @@
 
 require_once 'config.php';
 
-try {
-    // create db handle
-    $dbh = new PDO("mysql:host=$host;dbname=$db", $user, $pw);
+/**
+* DB
+*/
+class Database 
+{
 
+    public static $connection;
+
+    private $dbName = null, $dbHost = null, $dbPass = null, $dbUser = null;
+
+    private function __construct($dbDetails = array())
+    {
+        $this->dbName = $dbDetails['db_name'];
+        $this->dbHost = $dbDetails['db_host'];
+        $this->dbUser = $dbDetails['db_user'];
+        $this->dbPass = $dbDetails['db_pass'];
+
+        try {
+            $this->dbh = new PDO('mysql:host='.$this->dbHost.';dbname='.$this->dbName, $this->dbUser, $this->dbPass);
+
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+        $this->tarot_query($this->dbh);
+    }
+
+    public function tarot_query($dbh){
     // prepare and execute sql 
-    $statement = $dbh->prepare("SELECT * FROM tarot_cards");
-    $statement->execute();
+        $statement = $dbh->prepare("SELECT * FROM tarot_cards");
+        $statement->execute();
     // fetch query as associative array
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
     // encode query to json so angular can use it
-    $json = json_encode($results);
-    
-    echo $json;
-    
+        $json = json_encode($results);
+
+        echo $json;
+
     // close the database connection 
-    $dbh = null;
-}
-catch (PDOException $e) {
-    echo $e->getMessage();
+        $dbh = null;
+    }
+
+    public static function connect($dbDetails = array()){
+        if (!isset(self::$connection)) {
+            self::$connection = new Database($dbDetails);
+        } 
+        
+        return self::$connection;
+    }
+
 }
 
+$DB =  Database::connect($dbDetails);
+
+
+// $test = Database::connect();
+// echo $test;
 
 
 ?>
